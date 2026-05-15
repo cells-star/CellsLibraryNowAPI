5842579559c3b060bf6448bedcd71021e763de2e
Tricia Nicole Cells <cellstricianicole@gmail.com>
2026-05-15 01:04:23 -0800

    Program.cs

--- CellsLibraryNowAPI/Program.cs
+++ CellsLibraryNowAPI/Program.cs
@@ -1,18 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseAuthorization();
+app.MapGet("/", () => "CellsLibraryNowAPI is running");
 
 app.MapControllers();
-
-app.MapGet("/", () => "CellsLibraryNowAPI is running.");
 
 app.Run();
